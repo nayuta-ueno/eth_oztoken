0dc2d09dbcb23afe9673fcf8c7a3c100e353343d
ueno <ueno@nayuta.co>
2020-01-02 17:42:36 +0900

    truffleAssertがうまく使えなかった

--- test/TestOzToken.js
+++ test/TestOzToken.js
@@ -1,13 +1,20 @@
 const UToken = artifacts.require("./OzToken.sol");
+const truffleAssert = require('truffle-assertions');
 
 contract("OzToken test", async accounts => {
+  it("supply amount1", async () => {
+    let instance = await UToken.deployed();
+
+    let supply = await instance.totalSupply();
+    assert.equal(supply, 200000, "supply");
+  });
+
   it("deploy amount", async () => {
     let instance = await UToken.deployed();
-    let owner = accounts[0];
 
+    let owner = accounts[0];
     let balance = await instance.balanceOf(owner);
-    let expected = String(200000);
-    assert.equal(balance.toString(), expected);
+    assert.equal(balance, 200000, "balance");
   });
 
   it("direct transfer", async () => {
@@ -17,12 +24,79 @@ contract("OzToken test", async accounts => {
     let payee = accounts[1];
     let payAmt = 10000;
     await instance.transfer(payee, payAmt);
-    
+
     let balanceOwner = await instance.balanceOf(owner);
-    assert.equal(balanceOwner.toString(), String(200000 - payAmt), "balanceOwner");
-    
+    assert.equal(200000 - balanceOwner, payAmt, "balanceOwner");
+
     let balancePayee = await instance.balanceOf(payee);
-    assert.equal(balancePayee.toString(), String(payAmt), "balancePayee");
+    assert.equal(balancePayee, payAmt, "balancePayee");
+  });
+
+  it("approve transfer from owner", async () => {
+    let instance = await UToken.deployed();
+
+    let owner = accounts[0];
+    let payer = accounts[0];
+    let payee = accounts[1];
+    var balanceOwnerBegin = await instance.balanceOf(owner);
+    var balancePayerBegin = await instance.balanceOf(payer);
+    var balancePayeeBegin = await instance.balanceOf(payee);
+
+    let payAmt = 10000;
+
+    await instance.approve(payer, payAmt);
+    let appr = await instance.allowance(owner, payer);
+    assert.equal(appr, payAmt, "approval");
+
+    await instance.transferFrom(payer, payee, payAmt);
+
+    let balanceOwner = await instance.balanceOf(owner);
+    assert.equal(balanceOwner - balanceOwnerBegin, -payAmt, "balanceOwner");
+
+    let balancePayer = await instance.balanceOf(payer);
+    assert.equal(balancePayer - balancePayerBegin, -payAmt, "balancePayer");
+
+    let balancePayee = await instance.balanceOf(payee);
+    assert.equal(balancePayee - balancePayeeBegin, payAmt, "balancePayee");
+  });
+
+  // おそらくownerのコンテキスト？になっていて、別の人がapprove()するテストができない
+  // it("approve transfer from payer", async () => {
+  //   let instance = await UToken.deployed();
+
+  //   let owner = accounts[0];
+  //   let payer = accounts[1];
+  //   let payee = accounts[2];
+  //   var balanceOwnerBegin = await instance.balanceOf(owner);
+  //   var balancePayerBegin = await instance.balanceOf(payer);
+  //   var balancePayeeBegin = await instance.balanceOf(payee);
+
+  //   let payAmt = 5000;
+
+  //   assert.equal(balancePayerBegin > payAmt, true, "larger");
+
+  //   await instance.approve(payer, 2 * payAmt);
+  //   let balanceApprove = await instance.allowance(owner, payer);
+  //   console.log("balanceApprove=" + balanceApprove.toString());
+
+  //   await instance.transferFrom(payer, payee, payAmt);
+
+  //   // let balanceOwner = await instance.balanceOf(owner);
+  //   // assert.equal(balanceOwner - balanceOwnerBegin, 0, "balanceOwner");
+
+  //   // let balancePayer = await instance.balanceOf(payer);
+  //   // assert.equal(balancePayer - balancePayerBegin, -payAmt, "balancePayer");
+
+  //   // let balancePayee = await instance.balanceOf(payee);
+  //   // assert.equal(balancePayee - balancePayeeBegin, payAmt, "balancePayee");
+  // });
+
+  it("supply amount2", async () => {
+    let instance = await UToken.deployed();
+    let owner = accounts[0];
+
+    let supply = await instance.totalSupply();
+    assert.equal(supply, 200000, "supply");
   });
 
 });
