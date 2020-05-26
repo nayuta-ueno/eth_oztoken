310a0516f3515287fd09437213cdd5ab1699e212
ueno <ueno@nayuta.co>
2020-05-26 14:59:46 +0900

    OpenZeppelin@3.0.1

---
+++ README.md
@@ -0,0 +1,28 @@
+# eth_oztoken
+
+## prepare
+
+```console
+$ git clone https://github.com/nayuta-ueno/eth_oztoken.git
+$ cd eth_oztoken
+$ npm install @openzeppelin/contracts@0.3.1
+```
+
+## develop
+
+```console
+$ truffle develop
+
+> version
+Truffle v5.1.19 (core: 5.1.19)
+Solidity - 0.6.7 (solc-js)
+Node v13.12.0
+Web3.js v1.2.1
+
+> migrate
+```
+
+```console
+$ npm install web3
+$ node mytest.js
+```
--- mytest.js
+++ mytest.js
@@ -1,12 +1,12 @@
-const PROVIDER = 'http://10.0.1.80:8545';
-const CONTRACT_ADDR = '0x42EB8cb9ff23b7a57d99a80146B118c06b22c806';
+const PROVIDER = 'http://127.0.0.1:9545';
+const CONTRACT_ADDR = '<YOUR CONTRACT ADDRESS>';
 
 const fs = require('fs');
 var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(PROVIDER));
 
-async function async_func() {
+const fn = async () => {
 	console.log('Protocol Version: ' + await web3.eth.getProtocolVersion());
 	console.log('gas Price: ' + await web3.eth.getGasPrice());
 	console.log('balance of contract_addr=' + await web3.eth.getBalance(CONTRACT_ADDR));
@@ -21,4 +21,4 @@ async function async_func() {
 	let balance_token = await oztoken.methods.balanceOf(accounts[0]).call();
 	console.log('token=' + balance_token);
 }
-async_func();
+fn();
