2f6960995be4418039acf27ad76f169b3dccb180
ueno <ueno@nayuta.co>
2020-06-15 17:55:59 +0900

    SENDKEY

--- mytest2.js
+++ mytest2.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const Web3 = require('web3');
 
-// const PRIVATE_KEY = '<privateKey of SEND_ADDR>';
+// const PRIVATE_SENDKEY = '<privateKey of SEND_ADDR>';
 // const SEND_ADDR = '<sender address>';
 // const RECV_ADDR = 'receiver address';
 // const ENDPOINT = 'http://127.0.0.1:9545';
 
-const PRIVATE_KEY = '00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff';
+const PRIVATE_SENDKEY = '00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff';
 const SEND_ADDR = '0x87C018EF78005f118C53fa9cadf0a4Fd367a77A9';
-//const PRIVATE_RECVKEY = 'ca28013f3670b8a3b80be17ec1e774b16d0f8eb15f58a8a189f91166be358ca7';
+const PRIVATE_RECVKEY = 'ca28013f3670b8a3b80be17ec1e774b16d0f8eb15f58a8a189f91166be358ca7';
 const RECV_ADDR = '0xbb0b59396ba231c5122ccd9a6855ebd43286d431';
 const ENDPOINT = 'ws://127.0.0.1:21020';
 const ABI_FILE = './build/contracts/OzToken.json';
@@ -42,7 +42,7 @@ const fn = async () => {
     let gas = await method.estimateGas({ from: SEND_ADDR });
     console.log('estimateGas=' + gas);
 
-    nonce = await web3.eth.getTransactionCount(SEND_ADDR);
+    nonce = await web3.eth.getTransactionCount(RECV_ADDR);
     console.log('nonce=' + nonce);
 
     // transaction
@@ -55,7 +55,7 @@ const fn = async () => {
         gasPrice: gasPrice,
         gas: gas + 50000
     };
-    signtx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    signtx = await web3.eth.accounts.signTransaction(tx, PRIVATE_SENDKEY);
     console.log('signed_tx= ' + signtx.rawTransaction);
 
     // send transaction
